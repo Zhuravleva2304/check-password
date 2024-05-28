61a7625318be797c9b7021e4521f99782ab699d8
Zhuravleva2304 <zhuravlevan45@gmail.com>
2024-05-28 20:42:13 +0300

    branch1
    проверка пароля

---
+++ CheckThePassword.py
@@ -0,0 +1,15 @@
+import random
+
+def CheckThePassword(password):
+    isValid = False
+    if len(password) >= 8:
+        isValid = True
+    return isValid
+
+if __name__ == '__main__':
+    password = input("Please enter your passsword")
+    isValid = CheckThePassword(password)
+    if isValid:
+        print("Password is valid")
+    else:
+        print("Password is not valid")
